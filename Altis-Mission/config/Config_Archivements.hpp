085e88156870936c36b8f28f9cdb7a9648384479
Natic <37015273+Natic1@users.noreply.github.com>
2019-02-20 11:14:31 +0100

    Create Config_Archivements.hpp

---
+++ Altis-Mission/config/Config_Archivements.hpp
@@ -0,0 +1,29 @@
+class Archives
+  {
+    class jail {
+                class level_1 {
+                                needed = 100;
+                                archiv = "Straßenbulle";
+                    };
+                    
+                    class level_2 {
+                                needed = 200;
+                                archiv = "Krasser Bulle";
+                    };
+                    
+                    class level_3 {
+                                needed = 400;
+                                archiv = "Einknaster";
+                    };
+                    
+                    class level_4 {
+                                needed = 600;
+                                archiv = "Bulle von Kavala";
+                    };
+                    
+                    class level_5 {
+                                needed = 1000;
+                                archiv = "Bulle von Altis";
+                    };
+        };  
+  }
